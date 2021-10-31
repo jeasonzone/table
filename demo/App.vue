34bd9d343afcdbd270057bafdb02251b11135c01
jeasonzone <943745703@qq.com>
2021-10-31 20:49:47 +0800

    update

--- demo/App.vue
+++ demo/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main-container">
     <p>一、直接传入columns和rowsData</p>
-    <TestTable ref="table" :columns="arr" :rowsData="dataList" :elipse="true"></TestTable>
+    <TestTable :columns="arr" :rowsData="dataList" :elipse="true"></TestTable>
     <div class="container-demo-line"></div>
     <p>二、不展示表头</p>
     <TestTable :columns="arr" :rowsData="dataList" tableSize="easy" :elipse="true"></TestTable>
@@ -9,7 +9,6 @@
     <p>三、不展示分页</p>
     <TestTable :columns="arr" :rowsData="dataList" :isShowPaging="false"></TestTable>
     <div class="container-demo-line"></div>
-    <button @click="wwww">www</button>
   </div>
 </template>
 
@@ -44,10 +43,7 @@ export default defineComponent({
       { child: "小鬼", age: 3 },
       { child: "小狗", age: 22 },
     ]);
-    const wwww = () => {
-      console.log(table.value);
-    }
-    return { arr, dataList, table, wwww };
+    return { arr, dataList, table };
   },
 });
 </script>
